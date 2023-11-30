514953c6d4304cee38b462f5db19b1b6fcbc5326
Andrey0388 <zvi.anvel+@gmail.com>
2023-11-30 18:10:52 +0300

    Receptus

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import sqlite3
 import hashlib
 
 from PyQt5 import uic
-from PyQt5.QtWidgets import QApplication, QMainWindow, QMessageBox
+from PyQt5.QtWidgets import QApplication, QMainWindow, QMessageBox, QFileDialog
 
 
 class RegisterWindow(QMainWindow):
@@ -85,6 +85,7 @@ class MainWindow(QMainWindow):
         # Кнопки добавить и удалить открывают окна добавления и удаления рецепта
         self.addButton.clicked.connect(self.addRecept)
         self.deleteButton.clicked.connect(self.deleteRecept)
+        self.sendButton.clicked.connect(self.sendRecept)
 
         # Добавим юзера, его пароль и доступ юзера
         self.UserName = UserName
@@ -106,7 +107,10 @@ class MainWindow(QMainWindow):
         result = cur.execute("""SELECT * FROM Users
                                 WHERE Name = ? and Password = ?""", (self.UserName, self.UserPassword,)).fetchall()
         self.UserKeys = result[0][2]
-        self.UserKeys = list(map(int, self.UserKeys.split(',')))
+        if self.UserKeys:
+            self.UserKeys = list(map(int, self.UserKeys.split(',')))
+        else:
+            self.UserKeys = list()
 
         # открываем окно фильтров
         self.ex1 = FiltersWindow(self.recepts, self.UserKeys)
@@ -136,7 +140,10 @@ class MainWindow(QMainWindow):
                                         WHERE Name = ? and Password = ?""",
                              (self.UserName, self.UserPassword,)).fetchall()
         self.UserKeys = result[0][2]
-        self.UserKeys = list(map(int, self.UserKeys.split(',')))
+        if self.UserKeys:
+            self.UserKeys = list(map(int, self.UserKeys.split(',')))
+        else:
+            self.UserKeys = list()
 
         # Добавляем в поле рецептов все рецепты из базы данных
         # только если у данного юзера права на той или иной рецепт
@@ -154,7 +161,10 @@ class MainWindow(QMainWindow):
         result = cur.execute("""SELECT * FROM Users
                                 WHERE Name = ? and Password = ?""", (self.UserName, self.UserPassword,)).fetchall()
         self.UserKeys = result[0][2]
-        self.UserKeys = list(map(int, self.UserKeys.split(',')))
+        if self.UserKeys:
+            self.UserKeys = list(map(int, self.UserKeys.split(',')))
+        else:
+            self.UserKeys = list()
 
         self.Aw = AddWindow(self.UserName, self.UserPassword, self.UserKeys)
         self.Aw.show()
@@ -162,6 +172,10 @@ class MainWindow(QMainWindow):
     def deleteRecept(self):
         self.Dw = DeleteWindow(self.UserName, self.UserPassword, self.UserKeys)
         self.Dw.show()
+
+    def sendRecept(self):
+        self.Sw = SendWindow(self.UserName, self.UserPassword, self.UserKeys)
+        self.Sw.show()
 
 
 class FiltersWindow(QMainWindow):
@@ -345,25 +359,91 @@ class DeleteWindow(QMainWindow):
     def deleteRecept(self):
         # Рецепт который хочет удалить пользователь
         delete_recept = self.deleteBox.currentText()
-        # Спрашиваем у пользователя подтверждение на удаление элементов
-        valid = QMessageBox.question(self, '', "Действительно удалить рецепт " + delete_recept,
-                                     QMessageBox.Yes, QMessageBox.No)
-        # Если пользователь ответил утвердительно, удаляем доступ к данному рецепту.
-        if valid == QMessageBox.Yes:
-            con = sqlite3.connect("recepts_db.sqlite")
-            cur = con.cursor()
+        if delete_recept:
+            # Спрашиваем у пользователя подтверждение на удаление элементов
+            valid = QMessageBox.question(self, '', "Действительно удалить рецепт " + delete_recept,
+                                         QMessageBox.Yes, QMessageBox.No)
+            # Если пользователь ответил утвердительно, удаляем доступ к данному рецепту.
+            if valid == QMessageBox.Yes:
+                con = sqlite3.connect("recepts_db.sqlite")
+                cur = con.cursor()
 
-            # id delete_recept
-            result = cur.execute("""SELECT * FROM Recepts WHERE Name = ?""", (delete_recept,)).fetchall()
-            id_delete_recept = result[0][0]
-            # Новый доступ пользователя
-            self.UserKeys.remove(id_delete_recept)
-            new_keys = ','.join(list(map(str, self.UserKeys)))
-            # Изменяем доступ юзера по имени и паролю (они не бывают идентичные для двух пользователей)
-            result3 = cur.execute("""UPDATE Users SET Keys = ? WHERE Name = ? and Password = ?""",
-                                  (new_keys, self.UserName, self.UserPassword,))
-            con.commit()
-            self.close()
+                # id delete_recept
+                result = cur.execute("""SELECT * FROM Recepts WHERE Name = ?""", (delete_recept,)).fetchall()
+                id_delete_recept = result[0][0]
+                # Новый доступ пользователя
+                self.UserKeys.remove(id_delete_recept)
+                new_keys = ','.join(list(map(str, self.UserKeys)))
+                # Изменяем доступ юзера по имени и паролю (они не бывают идентичные для двух пользователей)
+                result3 = cur.execute("""UPDATE Users SET Keys = ? WHERE Name = ? and Password = ?""",
+                                      (new_keys, self.UserName, self.UserPassword,))
+                con.commit()
+                self.close()
+
+
+class SendWindow(QMainWindow):
+    def __init__(self, UserName, UserPassword, UserKeys):
+        super().__init__()
+        # Загружаем дизайн
+        uic.loadUi('send.ui', self)
+        self.setWindowTitle('SendRecept')
+        self.sendButton.clicked.connect(self.sendRecept)
+
+        # Добавим юзера
+        self.UserName = UserName
+        self.UserPassword = UserPassword
+        self.UserKeys = UserKeys
+
+        # Заполним бокс рецептами
+        con = sqlite3.connect("recepts_db.sqlite")
+        cur = con.cursor()
+        result = cur.execute("""SELECT * FROM Recepts""").fetchall()
+        for id, name, category, recept in result:
+            # Теперь добаивм в бокс все доступные пользователю рецепты
+            if id in self.UserKeys:
+                self.sendBox.addItem(name)
+        con.commit()
+
+    def sendRecept(self):
+        # Рецепт который хочет отправить пользователь
+        send_recept = self.sendBox.currentText()
+        sUser = self.sendEdit.text()
+        # Хэшируем строки
+        sUser = hashlib.sha1(sUser.encode("utf-8")).hexdigest()
+        # Открываем базу данных и проверяем на наличие данного пользователя
+        con = sqlite3.connect("recepts_db.sqlite")
+        cur = con.cursor()
+        result = cur.execute("""SELECT * FROM Users
+                            WHERE Name = ?""", (sUser,)).fetchall()
+        if result:
+            # Доступ юзера
+            if result[0][2]:
+                UserKeys = result[0][2].split(",")
+            else:
+                UserKeys = list()
+            # Спрашиваем у пользователя подтверждение на отправление элементов
+            valid = QMessageBox.question(self, '', "Действительно отправить рецепт " + send_recept, QMessageBox.Yes,
+                                         QMessageBox.No)
+            # Если пользователь ответил утвердительно
+            if valid == QMessageBox.Yes:
+                # Находим в базе данных рецепт
+                # и в переменную send_recept_key записываем выданный базой данных индивидуальный ей айди
+                result2 = cur.execute("""SELECT * FROM Recepts WHERE Name = ?""", (send_recept,)).fetchall()
+                send_recept_key = result2[0][0]
+                if send_recept_key not in UserKeys:
+                    # Теперь у юзера будет доступ это его прошлые ключи и новый ключ
+                    new_keys = ','.join(list(map(str, UserKeys + [send_recept_key])))
+                    # Изменяем доступ юзера по имени (он не бывает идентичный для двух пользователей)
+                    result3 = cur.execute("""UPDATE Users SET Keys = ? WHERE Name = ?""",
+                                          (new_keys, sUser,))
+                    con.commit()
+
+                self.Uw = UraWindow()
+                self.Uw.show()
+
+                self.close()
+        else:
+            self.statusbar.showMessage('Данного пользователя не существует!', 5000)
 
 
 if __name__ == '__main__':
